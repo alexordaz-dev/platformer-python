4cdfcbe5cd6d959cfc00da1db9571ba20f73e043
alexordaz <alex.olizundia@gmail.com>
2023-12-06 19:34:12 +0100

    Programming

--- board.py
+++ board.py
@@ -51,46 +51,10 @@ class Board:
         """ This is executed also each frame, here you should just draw
         things """
         pyxel.cls(0)
-        # We draw something putting blt taking the values from the  object
-        # Parameters are x, y, and a tuple containing the image bank,
-        # the starting x and y and the size which we initialize before with
-        # the self. ...
-        pyxel.blt(self.tuberias[0].x, self.tuberias[0].y, *self.tuberias[0].sprite)
-        pyxel.blt(self.tuberias[1].x, self.tuberias[1].y, *self.tuberias[1].sprite)
-        pyxel.blt(self.tuberias[2].x, self.tuberias[2].y, *self.tuberias[2].sprite)
-        pyxel.blt(self.tuberias[3].x, self.tuberias[3].y, *self.tuberias[3].sprite)
 
-        pyxel.blt(self.floor[0].x, self.floor[0].y, *self.floor[0].sprite)
-        pyxel.blt(self.floor[1].x, self.floor[1].y, *self.floor[1].sprite)
-        pyxel.blt(self.floor[2].x, self.floor[2].y, *self.floor[2].sprite)
-        pyxel.blt(self.floor[3].x, self.floor[3].y, *self.floor[3].sprite)
-        pyxel.blt(self.floor[4].x, self.floor[4].y, *self.floor[4].sprite)
-        pyxel.blt(self.floor[5].x, self.floor[5].y, *self.floor[5].sprite)
-        pyxel.blt(self.floor[6].x, self.floor[6].y, *self.floor[6].sprite)
-        pyxel.blt(self.floor[7].x, self.floor[7].y, *self.floor[7].sprite)
-        pyxel.blt(self.floor[8].x, self.floor[8].y, *self.floor[8].sprite)
-        pyxel.blt(self.floor[9].x, self.floor[9].y, *self.floor[9].sprite)
-        pyxel.blt(self.floor[10].x, self.floor[10].y, *self.floor[10].sprite)
-        pyxel.blt(self.floor[11].x, self.floor[11].y, *self.floor[11].sprite)
-        pyxel.blt(self.floor[12].x, self.floor[12].y, *self.floor[12].sprite)
-        pyxel.blt(self.floor[13].x, self.floor[13].y, *self.floor[13].sprite)
-        pyxel.blt(self.floor[14].x, self.floor[14].y, *self.floor[14].sprite)
-        pyxel.blt(self.floor[15].x, self.floor[15].y, *self.floor[15].sprite)
-        pyxel.blt(self.floor[16].x, self.floor[16].y, *self.floor[16].sprite)
-        pyxel.blt(self.floor2[0].x, self.floor2[0].y, *self.floor2[0].sprite)
-        pyxel.blt(self.floor2[1].x, self.floor2[1].y, *self.floor2[1].sprite)
-        pyxel.blt(self.floor2[2].x, self.floor2[2].y, *self.floor2[2].sprite)
-        pyxel.blt(self.floor2[3].x, self.floor2[3].y, *self.floor2[3].sprite)
-        pyxel.blt(self.floor2[4].x, self.floor2[4].y, *self.floor2[4].sprite)
-        pyxel.blt(self.floor2[5].x, self.floor2[5].y, *self.floor2[5].sprite)
-        pyxel.blt(self.floor2[6].x, self.floor2[6].y, *self.floor2[6].sprite)
-        pyxel.blt(self.floor2[7].x, self.floor2[7].y, *self.floor2[7].sprite)
-        pyxel.blt(self.floor2[8].x, self.floor2[8].y, *self.floor2[8].sprite)
-        pyxel.blt(self.floor2[9].x, self.floor2[9].y, *self.floor2[9].sprite)
-        pyxel.blt(self.floor2[10].x, self.floor2[10].y, *self.floor2[10].sprite)
-        pyxel.blt(self.floor2[11].x, self.floor2[11].y, *self.floor2[11].sprite)
-        pyxel.blt(self.floor2[12].x, self.floor2[12].y, *self.floor2[12].sprite)
-        pyxel.blt(self.floor2[13].x, self.floor2[13].y, *self.floor2[13].sprite)
-        pyxel.blt(self.floor2[14].x, self.floor2[14].y, *self.floor2[14].sprite)
-        pyxel.blt(self.floor2[15].x, self.floor2[15].y, *self.floor2[15].sprite)
-        pyxel.blt(self.floor2[16].x, self.floor2[16].y, *self.floor2[16].sprite)
+        for tuberia in self.tuberias:
+            pyxel.blt(tuberia.x, tuberia.y, *tuberia.sprite)
+
+            # Draw floor
+        for floor_tile in self.floor + self.floor2:
+            pyxel.blt(floor_tile.x, floor_tile.y, *floor_tile.sprite)
