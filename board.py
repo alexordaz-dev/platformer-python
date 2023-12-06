4d3ac1297e3f93e83764b40384c0cc5d5b01cdbc
alexordaz <alex.olizundia@gmail.com>
2023-12-06 22:42:52 +0100

    Programming

--- board.py
+++ board.py
@@ -20,25 +20,60 @@ class Board:
 
         # this creates the position of something in  the screen
         # example: self.mario = Mario(int(self.width / 2), 200)
-        self.tuberias = [Tuberias(0, 30, "left", "no_straight"),
-                      Tuberias(270, 30, "right", "no_straight"),
-                      Tuberias(0, 168, "left", "straight"),
-                      Tuberias(260, 168, "right", "straight")]
+        self.tuberias = [Tuberias(0, 20, "left", "no_straight"),
+                      Tuberias(270, 20, "right", "no_straight"),
+                      Tuberias(0, 175, "left", "straight"),
+                      Tuberias(260, 175, "right", "straight")]
         floor1 = []
         x = 0
-        for i in range(17):
-            floor1.append(Floor(x,63))
+        for i in range(18):
+            floor1.append(Floor(x,55))
             x += 7
         self.floor = floor1
 
         floor2 = []
-        x = 181
-        for i in range(17):
-            floor2.append(Floor(x,63))
+        x = 174
+        for i in range(18):
+            floor2.append(Floor(x,55))
             x += 7
         self.floor2 = floor2
-        # Running the game
+
+        floor3 = []
+        x = 94
+        for i in range(16):
+            floor3.append(Floor(x,100))
+            x += 7
+        self.floor3 = floor3
+
+        floor4 = []
+        x = 0
+        for i in range(6):
+            floor4.append(Floor(x,108))
+            x += 7
+        self.floor4 = floor4
+
+        floor5 = []
+        x = 258
+        for i in range(6):
+            floor5.append(Floor(x, 108))
+            x += 7
+        self.floor5 = floor5
+
+        floor6 = []
+        x = 0
+        for i in range(16):
+            floor6.append(Floor(x, 150))
+            x += 7
+        self.floor6 = floor6
+
+        floor7 = []
+        x = 188
+        for i in range(16):
+            floor7.append(Floor(x, 150))
+            x += 7
+        self.floor7 = floor7
         pyxel.run(self.update, self.draw)
+
 
     def update(self):
         """ This is executed each frame, here invocations to the update
@@ -56,5 +91,5 @@ class Board:
             pyxel.blt(tuberia.x, tuberia.y, *tuberia.sprite)
 
             # Draw floor
-        for floor_tile in self.floor + self.floor2:
+        for floor_tile in self.floor + self.floor2 + self.floor3 + self.floor4 + self.floor5 + self.floor6 + self.floor7:
             pyxel.blt(floor_tile.x, floor_tile.y, *floor_tile.sprite)
